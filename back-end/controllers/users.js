b21d366399b0c54f365aef1a6cc4c1ff244eea7c
JuanRCoder <guillecarck@gmail.com>
2023-11-11 11:59:15 -0500

    feat: server for connect to MongoDB

--- back-end/controllers/users.js
+++ back-end/controllers/users.js
@@ -1,20 +1,90 @@
+const user = require('../models/user');     //Exportar modelo
 const path = require('path');
-const user = require('../models/user');
 
-exports.getForm = (req, res) => {
-    res.sendFile(path.join(__dirname, '../../', 'index.html'));
-}
-
-exports.postForm = (req, res) => {
-    const {name, gmail, password} = req.body;
-
-    //new model
-    const  newUser = new user({name, gmail, password});
-    newUser.save();
-
-    res.redirect('/register');
+//GET:  TRAE PAGINA PRINCIPAL (main.html)
+exports.getPage = (req, res) => {
+    const htmlFilePath = path.join(__dirname, '../../front-end/main.html');
+    res.sendFile(htmlFilePath);
 };
 
-exports.getRegister = (req, res) => {
-    res.sendFile(path.join(__dirname, '../../', 'index.html'));
-}
+//GET: TRAE PAGINA FORM (index.html)
+exports.getForm = async (req, res) => {
+    const htmlFilePath = path.join(__dirname, '../../front-end/form.html');
+    res.sendFile(htmlFilePath);
+};
+
+//POST: ENVIA LOS DATOS A MONGODB  (REGISTRO)
+exports.sendForm = async (req, res) => {
+    const { name, gmail, password } = req.body;
+
+    try {
+        // Verificar si el nombre ya existe
+        const existingUserByName = await user.findOne({ name });
+        if (existingUserByName) {
+            return res.status(400).json({ mensaje: 'El nombre ya está en uso' });
+        }
+
+        // Verificar si el nombre ya existe
+        const existingUserByGmail = await user.findOne({ gmail });
+        if (existingUserByGmail) {
+            return res.status(400).json({ mensaje: 'El correo ya esta registrado' });
+        }
+
+        // Verificar si la contraseña existe
+        const existingUserByPassword = await user.findOne({ password });
+        if (existingUserByPassword) {
+            return res.status(400).json({ mensaje: 'La contraseña ya existe' });
+        }
+
+        // Si no hay usuarios con los mismos datos, guardar el nuevo usuario
+        const newUser = new user({ name, password });
+        await newUser.save();
+
+        res.status(201).json({ mensaje: 'Nuevo Usuario registrado con éxito' });
+
+    } catch (error) {
+        console.error('Error al guardar el usuario:', error);
+        res.status(500).json({ mensaje: 'Error al guardar el usuario' });
+    }
+};
+
+//POST: ENVIA LOS DATOS A MONGODB  (LOGIN)
+exports.sendFormLo = async (req, res) => {
+    const { name, password } = req.body;
+
+    try {
+        // Verificar si existe un usuario con el nombre y contraseña proporcionados
+        const existingUser = await user.findOne({ name, password });
+        if (!existingUser) {
+            return res.status(400).json({ mensaje: 'Credenciales incorrectas' });
+        }
+
+        res.status(200).json({ mensaje: 'Inicio de sesión exitoso' });
+    } catch (error) {
+        console.error('Error al verificar las credenciales:', error);
+        res.status(500).json({ mensaje: 'Error al verificar las credenciales' });
+    }
+};
+
+
+//GET: MOSTRAR LA PAGINA DE USUARIO
+exports.showPage = (req, res) => {
+    const htmlFilePath = path.join(__dirname, '../../front-end/form.html');
+    res.sendFile(htmlFilePath);
+};
+
+
+
+
+// Estrucutura del proyecto:
+// - Project-Login_Register_v2
+//     - back-end
+//         - server.js
+//     - front-end
+//         - styles
+//             - style.css
+//             - imagenes
+//             - scritp.js
+//             - scritp2.js
+//         - index.html
+//         - main.html
--- back-end/server.js
+++ back-end/server.js
@@ -1,20 +1,27 @@
 const express = require('express');
-const routers = require('./routers');
 const connectDB = require('./config/db');
+const userController = require('./controllers/users');
 const path = require('path');
 
 const app = express();
 const port = 3002;
 
+
+// Configura Express para servir archivos estáticos desde el directorio 'styles'
+app.use('/styles', express.static(path.join(__dirname, '../front-end/styles')));
+
+
+// Parsear las URL con contenido JSON
 app.use(express.json());
 
-// Configura Express para servir archivos estáticos desde el directorio raíz del proyecto
-// app.use(express.static(path.join(__dirname, '../')));
-app.use(express.static(path.join(__dirname, '../')));
-
-
 // Usa las rutas para la aplicación Express
-app.use(routers);
+app.get('/', userController.getPage);                           // get pageMain
+app.get('/form', userController.getForm);                       // get formulario
+app.post('/form/register', userController.sendForm);            // post formulario(registro)
+app.post('/form/login', userController.sendFormLo);             // post formulario(login)
+
+app.get('/form/register', userController.showPage);               // PAGINA DE USUARIO
+
 
 app.listen(port, () => {
     console.log(`Aplicacion Express en linea en el puerto ${port}`);
@@ -22,23 +29,3 @@ app.listen(port, () => {
 
 // Conectar a MongoDB
 connectDB();
-
-
-// -Project-Login_Register_v2
-//     - back-end
-//         -config
-//             - db.js
-//         -controllers
-//             - user.js
-//         -models
-//             - user.js
-//         -routers.js
-//         -server.js
-//     - images
-//         - registro.ico
-//         - google.png
-//         - gorjeo.png
-//         - instagram.png
-//     - index.html
-//     - script.js
-//     - style.css
